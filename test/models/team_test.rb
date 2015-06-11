227828202e9c70c459b12352489c97065e9bc577
Dan Madere <danmadere@gmail.com>
2015-06-11 00:23:02 -0400

    Team unit tests

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class TeamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should require name" do
+    team = teams(:team)
+    team.name = nil
+    assert_not team.save
+  end
+
+  test "should require data name" do
+    team = teams(:team)
+    team.data_name = nil
+    assert_not team.save
+  end
+
+  test "games method should return array of games" do
+    games = teams(:team).games
+    #TODO: figure out what assertions to do here
+  end
 end
