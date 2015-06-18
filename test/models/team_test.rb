632d9185e62d6d07b0e8358d10fd8436849ccc8d
Dan Madere <danmadere@gmail.com>
2015-06-17 23:32:28 -0400

    Team unit tests

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -2,19 +2,21 @@ require 'test_helper'
 
 class TeamTest < ActiveSupport::TestCase
   test "should require name" do
-    team = teams(:team)
+    team = teams(:one)
     team.name = nil
     assert_not team.save
   end
 
   test "should require data name" do
-    team = teams(:team)
+    team = teams(:one)
     team.data_name = nil
     assert_not team.save
   end
 
-  test "games method should return array of games" do
-    games = teams(:team).games
-    #TODO: figure out what assertions to do here
+  test "games method should return activerecord association to games" do
+    games = teams(:one).games
+    assert_instance_of Game::ActiveRecord_Relation, games
+    assert_equal games.count, 2
   end
+
 end
