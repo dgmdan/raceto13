c15120815d708079e7486f1ff01578400b3b4051
Dan Madere <danmadere@gmail.com>
2015-06-17 22:23:21 -0400

    Games functional tests

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class GamesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::TestHelpers
+
+  test "should not allow non-admin users" do
+    sign_in users(:user)
+    get :index
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should get index" do
+    sign_in users(:admin)
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:games)
+  end
+
+  test "should get mass entry form" do
+    #TODO: implement this test
+  end
+
+  test "should mass create games" do
+    #TODO: implement this test
+  end
 end
