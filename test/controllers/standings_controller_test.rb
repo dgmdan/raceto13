5fa3aad82cc558698cb03b9d554cfd5775324956
Dan Madere <danmadere@gmail.com>
2015-06-17 22:27:16 -0400

    standings functional tests

--- test/controllers/standings_controller_test.rb
+++ test/controllers/standings_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class StandingsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::TestHelpers
+
+  test "don't allow unauthenticated visitors" do
+    get :index
+    assert_redirected_to new_user_session_path
+  end
+
+  test "gets index" do
+    sign_in users(:user)
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:entries)
+    assert_not_nil assigns(:run_counts)
+  end
+
 end
