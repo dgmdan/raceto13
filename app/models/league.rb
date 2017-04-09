a2ad53c690f48533af5805f25e466f39fa15214f
Dan Madere <dan@danmadere.com>
2017-04-09 19:02:21 -0400

    populate entries.game_count when winners are chosen

--- app/models/league.rb
+++ app/models/league.rb
@@ -35,7 +35,7 @@ class League < ActiveRecord::Base
           FROM entries
           JOIN hits ON hits.entry_id = entries.id
           JOIN teams ON teams.id = entries.team_id
-          JOIN games ON (games.home_team_id = teams.id OR games.away_team_id = teams.id)
+          JOIN games ON (games.home_team_id = teams.id OR games.away_team_id = teams.id) AND DATE(games.started_on) <= '#{query_date}'
           WHERE entries.league_id = #{id}
           AND entries.cancelled_at IS NULL
           AND entries.won_at IS NULL
@@ -48,19 +48,19 @@ class League < ActiveRecord::Base
 
     # Get all first-place winners
     if first_counts_results.count > 0
-      winning_hits = first_counts_results.first['hits']
-      winning_game_count = first_counts_results.first['game_count']
+      first_winning_hits = first_counts_results.first['hits']
+      first_winning_game_count = first_counts_results.first['game_count']
       first_winners_sql = """
       SELECT entries.id entry_id
           FROM entries
           JOIN hits ON hits.entry_id = entries.id
           JOIN teams ON teams.id = entries.team_id
-          JOIN games ON (games.home_team_id = teams.id OR games.away_team_id = teams.id)
+          JOIN games ON (games.home_team_id = teams.id OR games.away_team_id = teams.id) AND DATE(games.started_on) <= '#{query_date}'
           WHERE entries.league_id = #{id}
           AND entries.cancelled_at IS NULL
           AND entries.won_at IS NULL
           GROUP BY entries.id
-          HAVING COUNT(DISTINCT hits.id) = #{winning_hits} AND COUNT(DISTINCT games.id) = #{winning_game_count};
+          HAVING COUNT(DISTINCT hits.id) = #{first_winning_hits} AND COUNT(DISTINCT games.id) = #{first_winning_game_count};
       """
       first_winners = ActiveRecord::Base.connection.execute(first_winners_sql)
       first_entry_ids = first_winners.map { |x| x['entry_id'] }
@@ -72,7 +72,7 @@ class League < ActiveRecord::Base
           FROM entries
           JOIN hits ON hits.entry_id = entries.id
           JOIN teams ON teams.id = entries.team_id
-          JOIN games ON (games.home_team_id = teams.id OR games.away_team_id = teams.id)
+          JOIN games ON (games.home_team_id = teams.id OR games.away_team_id = teams.id) AND DATE(games.started_on) <= '#{query_date}'
           WHERE entries.league_id = #{id}
           AND entries.cancelled_at IS NULL
           AND entries.won_at IS NULL
@@ -82,19 +82,19 @@ class League < ActiveRecord::Base
           LIMIT 1;
       """
       second_counts_results = ActiveRecord::Base.connection.execute(second_counts_sql)
-      winning_hits = second_counts_results.first['hits']
-      winning_game_count = second_counts_results.first['game_count']
+      second_winning_hits = second_counts_results.first['hits']
+      second_winning_game_count = second_counts_results.first['game_count']
       second_winners_sql = """
       SELECT entries.id entry_id
           FROM entries
           JOIN hits ON hits.entry_id = entries.id
           JOIN teams ON teams.id = entries.team_id
-          JOIN games ON (games.home_team_id = teams.id OR games.away_team_id = teams.id)
+          JOIN games ON (games.home_team_id = teams.id OR games.away_team_id = teams.id) AND DATE(games.started_on) <= '#{query_date}'
           WHERE entries.league_id = #{id}
           AND entries.cancelled_at IS NULL
           AND entries.won_at IS NULL
           GROUP BY entries.id
-          HAVING COUNT(DISTINCT hits.id) = #{winning_hits} AND COUNT(DISTINCT games.id) = #{winning_game_count};
+          HAVING COUNT(DISTINCT hits.id) = #{second_winning_hits} AND COUNT(DISTINCT games.id) = #{second_winning_game_count};
       """
       second_winners = ActiveRecord::Base.connection.execute(second_winners_sql)
 
@@ -103,6 +103,7 @@ class League < ActiveRecord::Base
         entry = Entry.find(result['entry_id'])
         entry.won_at = Time.now
         entry.won_place = 1
+        entry.game_count = first_winning_game_count
         entry.save
         winners << entry
       }
@@ -110,8 +111,15 @@ class League < ActiveRecord::Base
         entry = Entry.find(result['entry_id'])
         entry.won_at = Time.now
         entry.won_place = 2
+        entry.game_count = second_winning_game_count
         entry.save
         winners << entry
+      }
+
+      # Set games played on losing entries
+      self.losers.each { |entry|
+        entry.game_count = entry.team.games.where('DATE(started_on) <= ?', query_date).count
+        entry.save
       }
     end
     return winners
