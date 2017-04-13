7c8a9173036d20bbd2a0d2fa05284a364bc458db
Dan Madere <dan@danmadere.com>
2017-04-12 22:29:29 -0400

    set up team.games relation

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,6 +3,8 @@ class Team < ActiveRecord::Base
   validates :name, :data_name, presence: true
   validates :data_name, uniqueness: true
 
+  has_many :games
+
   def games
     Game.where(["home_team_id = ? OR away_team_id = ?", id, id])
   end
