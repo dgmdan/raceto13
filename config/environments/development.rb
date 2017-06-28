e67e2ee33415ec5e5427f53926736b4ae8af6f46
Dan Madere <dan@danmadere.com>
2017-06-28 17:16:54 -0400

    log outgoing email to file on dev environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,9 +25,11 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.delivery_method = :file
+  config.action_mailer.file_settings = { :location => '/tmp/rails-mail' }
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.asset_host = 'https://raceto13.com/images'
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
   config.log_level = :warn
