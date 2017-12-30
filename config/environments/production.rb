73aac6d31685821ad9888253c8ea88d0c8b143f7
Dan Madere <dan@danmadere.com>
2017-12-30 09:47:23 -0500

    enable asset serving on prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  # Serve static assets
+  config.public_file_server.enabled = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
