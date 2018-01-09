4eb42ed794412cbc2f6d137be9e52d8cfcaad58c
Dan Madere <dan@danmadere.com>
2018-01-08 22:28:54 -0500

    if new user passes a valid invite uuid, associate them to league

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,18 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def create
+    super
+
+    # if new user passes a valid invite uuid, associate them to league
+    if resource.save and params[:invite_uuid]
+      leagues = League.where(invite_uuid: params[:invite_uuid])
+      if leagues.any?
+        league = leagues.first
+        LeagueUser.create!(user: resource, league: league)
+      end
+    end
+  end
+
   protected
 
   def update_resource(resource, params)
