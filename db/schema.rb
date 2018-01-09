287ff8209a3bb06a58f0f732fe24aaa7bd43d0fb
Dan Madere <dan@danmadere.com>
2018-01-08 19:55:51 -0500

    schema.rb update from adding unique on league_users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180107213935) do
+ActiveRecord::Schema.define(version: 20180109003447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20180107213935) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["league_id"], name: "index_league_users_on_league_id"
+    t.index ["user_id", "league_id"], name: "index_league_users_on_user_id_and_league_id", unique: true
     t.index ["user_id"], name: "index_league_users_on_user_id"
   end
 
