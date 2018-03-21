8c91b33d227a27c5a0afa36ace8c50df80ac5afc
Dan Madere <dan@danmadere.com>
2018-03-20 21:15:53 -0400

    add sidekiq-cron and schedule the score scraping job

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,6 +5,10 @@ redis_config.merge! redis_config.fetch(Rails.env, {})
 redis_config.symbolize_keys!
 Sidekiq.configure_server do |config|
 	config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
+  schedule_file = "config/sidekiq_schedule.yml"
+  if File.exists?(schedule_file) && Sidekiq.server?
+    Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
+  end
 end
 Sidekiq.configure_client do |config|
 	config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
