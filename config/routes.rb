55c2b4c73a91867e076444c110c365d4d3942cce
Dan Madere <dan@danmadere.com>
2022-11-14 15:53:23 -0500

    move things where asset pipeline expects it

--- config/routes.rb
+++ config/routes.rb
@@ -46,7 +46,7 @@ Rails.application.routes.draw do
   end
 
   # authentication
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users, path: 'registrations'
 
   # rails console
   get 'console' => 'application#console'
