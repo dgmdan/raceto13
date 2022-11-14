c3cdf330e41f7abe745a03ec122af04dea3ee70f
Dan Madere <dan@danmadere.com>
2022-11-14 15:03:07 -0500

    bugfix when running `rake db:seed`

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-NotificationType.find_or_create_by(name: 'hit', description: 'When my score increases')
-NotificationType.find_or_create_by(name: 'conclude', description: 'When the pool ends')
+NotificationType.find_or_create_by(name: 'hit', description: 'When my score increases', created_at: DateTime.now, updated_at: DateTime.now)
+NotificationType.find_or_create_by(name: 'conclude', description: 'When the pool ends', created_at: DateTime.now, updated_at: DateTime.now)
