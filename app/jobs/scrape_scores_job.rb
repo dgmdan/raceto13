6010a203820260daa1626ca885f458bad0b066b5
Dan Madere <dan@danmadere.com>
2024-04-20 19:15:04 -0400

    comment nit

--- app/jobs/scrape_scores_job.rb
+++ app/jobs/scrape_scores_job.rb
@@ -7,7 +7,7 @@ class ScrapeScoresJob < ApplicationJob
 
   def perform(*_args)
     last_game_date = Game.any? ? Game.maximum('started_on') : (Date.today - 1)
-    # There are days with no MLB games, so we need to keep advancing the date and trying again until we reach the current date
+    # some days have zero MLB games, so we keep advancing the date until we reach the current date
     last_game_date.upto(Date.today - 1) do |current_date|
       GameState.scrape_games!(current_date)
     end
