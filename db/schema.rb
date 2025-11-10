94cca8f4ff3edabb83c2d6d3a15b11c8083597ff
Dan Madere <dan@danmadere.com>
2025-11-09 20:56:43 -0500

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,113 +10,207 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_406_150_109) do
-  create_table 'entries', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'league_id'
-    t.integer 'team_id'
-    t.datetime 'paid_at', precision: nil
-    t.datetime 'cancelled_at', precision: nil
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.datetime 'won_at', precision: nil
-    t.integer 'won_place'
-    t.integer 'game_count'
-    t.index ['league_id'], name: 'index_entries_on_league_id'
-    t.index ['team_id'], name: 'index_entries_on_team_id'
-    t.index ['user_id'], name: 'index_entries_on_user_id'
+ActiveRecord::Schema[7.2].define(version: 2025_11_10_014149) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "entries", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "league_id"
+    t.bigint "team_id"
+    t.datetime "paid_at", precision: nil
+    t.datetime "cancelled_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "won_at", precision: nil
+    t.integer "won_place"
+    t.integer "game_count"
+    t.index ["league_id"], name: "index_entries_on_league_id"
+    t.index ["team_id"], name: "index_entries_on_team_id"
+    t.index ["user_id"], name: "index_entries_on_user_id"
   end
 
-  create_table 'games', force: :cascade do |t|
-    t.date 'started_on'
-    t.integer 'home_team_id'
-    t.integer 'away_team_id'
-    t.integer 'home_score'
-    t.integer 'away_score'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "games", force: :cascade do |t|
+    t.date "started_on"
+    t.integer "home_team_id"
+    t.integer "away_team_id"
+    t.integer "home_score"
+    t.integer "away_score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'hits', force: :cascade do |t|
-    t.integer 'entry_id'
-    t.integer 'runs'
-    t.date 'hit_on'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'game_id'
-    t.index ['entry_id'], name: 'index_hits_on_entry_id'
-    t.index ['game_id'], name: 'index_hits_on_game_id'
+  create_table "good_job_batches", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "description"
+    t.jsonb "serialized_properties"
+    t.text "on_finish"
+    t.text "on_success"
+    t.text "on_discard"
+    t.text "callback_queue_name"
+    t.integer "callback_priority"
+    t.datetime "enqueued_at"
+    t.datetime "discarded_at"
+    t.datetime "finished_at"
+    t.datetime "jobs_finished_at"
   end
 
-  create_table 'league_users', force: :cascade do |t|
-    t.integer 'league_id'
-    t.integer 'user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['league_id'], name: 'index_league_users_on_league_id'
-    t.index ['user_id'], name: 'index_league_users_on_user_id'
+  create_table "good_job_executions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.uuid "active_job_id", null: false
+    t.text "job_class"
+    t.text "queue_name"
+    t.jsonb "serialized_params"
+    t.datetime "scheduled_at"
+    t.datetime "finished_at"
+    t.text "error"
+    t.integer "error_event", limit: 2
+    t.text "error_backtrace", array: true
+    t.uuid "process_id"
+    t.interval "duration"
+    t.index ["active_job_id", "created_at"], name: "index_good_job_executions_on_active_job_id_and_created_at"
+    t.index ["process_id", "created_at"], name: "index_good_job_executions_on_process_id_and_created_at"
   end
 
-  create_table 'leagues', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.datetime 'starts_at', precision: nil
-    t.datetime 'ends_at', precision: nil
-    t.string 'invite_uuid'
-    t.index ['invite_uuid'], name: 'index_leagues_on_invite_uuid'
-    t.index ['user_id'], name: 'index_leagues_on_user_id'
+  create_table "good_job_processes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.jsonb "state"
+    t.integer "lock_type", limit: 2
   end
 
-  create_table 'notification_type_users', force: :cascade do |t|
-    t.integer 'notification_type_id'
-    t.integer 'user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['notification_type_id'], name: 'index_notification_type_users_on_notification_type_id'
-    t.index ['user_id'], name: 'index_notification_type_users_on_user_id'
+  create_table "good_job_settings", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "key"
+    t.jsonb "value"
+    t.index ["key"], name: "index_good_job_settings_on_key", unique: true
   end
 
-  create_table 'notification_types', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'description'
+  create_table "good_jobs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.text "queue_name"
+    t.integer "priority"
+    t.jsonb "serialized_params"
+    t.datetime "scheduled_at"
+    t.datetime "performed_at"
+    t.datetime "finished_at"
+    t.text "error"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.uuid "active_job_id"
+    t.text "concurrency_key"
+    t.text "cron_key"
+    t.uuid "retried_good_job_id"
+    t.datetime "cron_at"
+    t.uuid "batch_id"
+    t.uuid "batch_callback_id"
+    t.boolean "is_discrete"
+    t.integer "executions_count"
+    t.text "job_class"
+    t.integer "error_event", limit: 2
+    t.text "labels", array: true
+    t.uuid "locked_by_id"
+    t.datetime "locked_at"
+    t.index ["active_job_id", "created_at"], name: "index_good_jobs_on_active_job_id_and_created_at"
+    t.index ["batch_callback_id"], name: "index_good_jobs_on_batch_callback_id", where: "(batch_callback_id IS NOT NULL)"
+    t.index ["batch_id"], name: "index_good_jobs_on_batch_id", where: "(batch_id IS NOT NULL)"
+    t.index ["concurrency_key", "created_at"], name: "index_good_jobs_on_concurrency_key_and_created_at"
+    t.index ["concurrency_key"], name: "index_good_jobs_on_concurrency_key_when_unfinished", where: "(finished_at IS NULL)"
+    t.index ["cron_key", "created_at"], name: "index_good_jobs_on_cron_key_and_created_at_cond", where: "(cron_key IS NOT NULL)"
+    t.index ["cron_key", "cron_at"], name: "index_good_jobs_on_cron_key_and_cron_at_cond", unique: true, where: "(cron_key IS NOT NULL)"
+    t.index ["finished_at"], name: "index_good_jobs_jobs_on_finished_at", where: "((retried_good_job_id IS NULL) AND (finished_at IS NOT NULL))"
+    t.index ["job_class"], name: "index_good_jobs_on_job_class"
+    t.index ["labels"], name: "index_good_jobs_on_labels", where: "(labels IS NOT NULL)", using: :gin
+    t.index ["locked_by_id"], name: "index_good_jobs_on_locked_by_id", where: "(locked_by_id IS NOT NULL)"
+    t.index ["priority", "created_at"], name: "index_good_job_jobs_for_candidate_lookup", where: "(finished_at IS NULL)"
+    t.index ["priority", "created_at"], name: "index_good_jobs_jobs_on_priority_created_at_when_unfinished", order: { priority: "DESC NULLS LAST" }, where: "(finished_at IS NULL)"
+    t.index ["priority", "scheduled_at"], name: "index_good_jobs_on_priority_scheduled_at_unfinished_unlocked", where: "((finished_at IS NULL) AND (locked_by_id IS NULL))"
+    t.index ["queue_name", "scheduled_at"], name: "index_good_jobs_on_queue_name_and_scheduled_at", where: "(finished_at IS NULL)"
+    t.index ["scheduled_at"], name: "index_good_jobs_on_scheduled_at", where: "(finished_at IS NULL)"
   end
 
-  create_table 'teams', force: :cascade do |t|
-    t.string 'name'
-    t.string 'data_name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['data_name'], name: 'index_teams_on_data_name', unique: true
+  create_table "hits", force: :cascade do |t|
+    t.bigint "entry_id"
+    t.integer "runs"
+    t.date "hit_on"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "game_id"
+    t.index ["entry_id"], name: "index_hits_on_entry_id"
+    t.index ["game_id"], name: "index_hits_on_game_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at', precision: nil
-    t.datetime 'remember_created_at', precision: nil
-    t.integer 'sign_in_count', default: 0, null: false
-    t.datetime 'current_sign_in_at', precision: nil
-    t.datetime 'last_sign_in_at', precision: nil
-    t.string 'current_sign_in_ip'
-    t.string 'last_sign_in_ip'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.boolean 'admin', default: false
-    t.string 'name'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "league_users", force: :cascade do |t|
+    t.bigint "league_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["league_id"], name: "index_league_users_on_league_id"
+    t.index ["user_id"], name: "index_league_users_on_user_id"
   end
 
-  add_foreign_key 'entries', 'leagues'
-  add_foreign_key 'entries', 'teams'
-  add_foreign_key 'entries', 'users'
-  add_foreign_key 'hits', 'entries'
-  add_foreign_key 'league_users', 'leagues'
-  add_foreign_key 'league_users', 'users'
-  add_foreign_key 'leagues', 'users'
+  create_table "leagues", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "starts_at", precision: nil
+    t.datetime "ends_at", precision: nil
+    t.string "invite_uuid"
+    t.index ["invite_uuid"], name: "index_leagues_on_invite_uuid"
+    t.index ["user_id"], name: "index_leagues_on_user_id"
+  end
+
+  create_table "notification_type_users", force: :cascade do |t|
+    t.bigint "notification_type_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["notification_type_id"], name: "index_notification_type_users_on_notification_type_id"
+    t.index ["user_id"], name: "index_notification_type_users_on_user_id"
+  end
+
+  create_table "notification_types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "description"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.string "data_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["data_name"], name: "index_teams_on_data_name", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
+    t.string "name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "entries", "leagues"
+  add_foreign_key "entries", "teams"
+  add_foreign_key "entries", "users"
+  add_foreign_key "hits", "entries"
+  add_foreign_key "league_users", "leagues"
+  add_foreign_key "league_users", "users"
+  add_foreign_key "leagues", "users"
 end
